840c453b2018e7898eec0aed034157421013ee31
parth <chnderachanderaparth986@gmail.com>
2023-08-25 17:34:20 +0530

    js

---
+++ string trim/ex1.js
@@ -0,0 +1,8 @@
+let a = "           my name is ram           "; 
+// console.log(a.length);
+// a = a.trim();
+// console.log(a.length);
+// a = a.trimStart();
+// console.log(a.length);
+ a = a.trimEnd();
+ console.log(a.length);
