d0ce68f5dadc9232862ee6801c3c29da90f3c015
parth <chnderachanderaparth986@gmail.com>
2023-09-04 14:34:39 +0530

    js

--- bsice fun/dom.js
+++ bsice fun/dom.js
@@ -1,16 +1,18 @@
+//1
 // const square = function (number) {
 //     return number * number;
 // };
 // const x = square(4); 
 // console.log(x);
 
+//2
 // const cube = function (number) {
 //     return number * number * number;
 // };
 // const z = cube(4); 
 // console.log(z);
 
-
+//3
 // let voteing = function(n)
 // {
 	
@@ -21,4 +23,17 @@
 // }
 
 // console.log(n);	
+
+
+//4
+// let rank = function(){
+// if(34){
+ 
+//  console.log("pass");
+//}
+// else
+// {
+//     console.log("fall");
+// }
+// }
 
