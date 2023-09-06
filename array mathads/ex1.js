d1e523a3f6a5fee424099b5e6ce521b39771cf46
parth <chnderachanderaparth986@gmail.com>
2023-09-06 16:24:57 +0530

    js

--- array mathads/ex1.js
+++ array mathads/ex1.js
@@ -1,4 +1,4 @@
-// array mathds.
+// * array mathds. * //
 
 // 1. length => return a length of Array.
 // 2. push => Enter the element at Array of end position.
@@ -9,7 +9,7 @@
 // 7. sort => sorting the array.
 // 8. reverse => reverse array.
 
-// 1. length
+// 1. length //
 
 // let arr = (1,2,3,4,5, 1.3,2.5,3.2,4.5,5.6, Ram,Sita,Gita,Rhula,Sam);
 // let arr = (1,2,3,4,5);
@@ -24,7 +24,7 @@
 // console.log(arr[30]);
 // console.log(arr.length);
 
-// 2. push
+// 2. push //
 
 // let arr = [10,20,30,40,50];
 // console.log(arr); 
@@ -32,7 +32,7 @@
 // console.log(arr);
 // console.log(arr.length);
 
-// 3. pop
+// 3. pop //
 
 // let fruts = ['mango', 'kevi', 'garsp', 'bnana'];
 // console.log(fruts.pop());
@@ -40,27 +40,27 @@
 // fruts.pop();
 // console.log(fruts);
 
-// 4. shift
+// 4. shift //
 
 // let array1 = [1, 2, 3];
 // let firstElement = array1.shift();
 // console.log(array1);
 // console.log(firstElement);
 
-// 5. unshift
+// 5. unshift //
 
 // let array1 = [1, 2, 3];
 // console.log(array1.unshift(4, 5));
 // console.log(array1);
 
-// 6. concat
+// 6. concat //
 
 // let str1 = 'Hello';
 // let str2 = 'World';
 // console.log(str1.concat(' ', str2));
 // console.log(str2.concat(', ', str1));
 
-// 7. sort
+// 7. sort //
 
 // let months = ['March', 'Jan', 'Feb', 'Dec'];
 // months.sort();
@@ -69,7 +69,7 @@
 // array1.sort();
 // console.log(array1);
 
-// 8. reverse
+// 8. reverse //
 
 // let array1 = ['one', 'two', 'three'];
 // console.log('array1:', array1);
