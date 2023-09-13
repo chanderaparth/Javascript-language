29029b9259b9fe1ac0e00e65703f1537905594e0
parth <chnderachanderaparth986@gmail.com>
2023-09-13 17:54:18 +0530

    js

---
+++ for in loop and for of loop/ex1.js
@@ -0,0 +1,39 @@
+
+// 1. for in loop //
+//  let person = {
+//        name: "Sambhu",
+//        email: "Sambhu@Har.in",
+//        age:25,
+// };
+
+// console.log(Object.keys(person).length);
+
+//    for (let i in person) {                     
+//                console.log(person[i]);
+
+//     }; 
+
+
+// // let abc = ["One", "Two", "Three", "Four"];
+// //            for (let x in abc){ 
+// //                    console.log(abc[x]);
+// // }
+
+
+// let myString = "who are you ??..........";
+
+// for (let i in myString) { 
+//     console.log(myString[i]); }
+//  for(let x of myString) {
+// console.log(x);
+
+//  }
+
+
+// 2.  For of loop //
+
+// let abc = ["One", "Two", "Three", "Four"];
+
+// for (let x of abc){
+//            console.log(x);
+// };
