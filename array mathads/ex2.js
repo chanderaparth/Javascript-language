872b3420726c73d5935aae832c4f75d0a04a0bdb
parth <chnderachanderaparth986@gmail.com>
2023-09-13 17:31:58 +0530

    js

---
+++ array mathads/ex2.js
@@ -0,0 +1,84 @@
+// 9. max mathad //
+
+// function arraymax(arr){
+//   return Math.max.apply(null, arr);
+
+// }
+// let num = [10, 40, 60, 8, 33, 450];
+
+// console.log(arraymax(num));
+
+// 10. min mathad // 
+
+// function arraymin(arr){
+//   return Math.min.apply(null, arr);
+
+// }
+// let num = [10, 40, 60, 8, 33, 450];
+
+// console.log(arraymin(num));
+
+// 11. forEach mathad // 
+
+// let numbers = [45, 4, 9, 16, 25];
+// let txt = "";
+
+// numbers.forEach((value)=> {
+
+// txt += value + " ";
+// })
+// console.log(txt);
+
+// 12. map mathad // 
+
+// let num = [10, 40, 60, 8, 33, 450];
+
+// let br = num.map((val) => {
+
+//     return val * 4;
+// })
+// console.log(br);
+
+// 13. splice mathad // 
+
+// splice (position, removeElements, optional)
+// let num = [45, 12, 5, 59, 100, 26, 60]; 
+// num.splice (2, 0, "hello");
+// num.splice (2, 3, "hello");
+// num.splice (2, 3);
+// console.log(num)
+
+// 14. slice mathad // 
+
+// let num = [45, 12, 5, 59, 100, 26, 60]; 
+// // let dvd = num.slice(2);
+// let dvd = num.slice(2,6);
+// console.log(dvd);
+
+// 15. filtar mathad // 
+
+// let num = [45, 12, 5, 59, 100, 26, 60]; 
+// let txt = num.filter((val) =>{
+
+//     return val<30;
+
+// })
+// console.log(txt);
+
+// 16. reduce mathad / 17. reduceRight mathad // \
+
+// 1. //
+// let num= [45, 12, 5, 59, 100, 26, 60];
+// let txt = num. reduceRight((total, val) => {
+//   return total + val;
+// })
+// console.log(txt);
+
+// 2. //
+// let num= [45, 12, 5, 59, 100, 26, 60];
+// let txt = num. reduce((total, val) => {
+//   return total + val;
+// })
+// console.log(txt);
+
+// 18. find method //
---
+++ object creyated/ex1.js
@@ -0,0 +1,74 @@
+// let data = {
+
+//      Name: "Ram",
+//      Age: 20,
+//      Number: 9875645865, 
+// }
+// console.log(data.Name);
+// console.log(data.Age);
+// console.log(data.Number);
+// console.log(data);
+
+
+
+// 1. creating a object a variable
+//  let person = {
+//     name: "sanju",
+//     age: 25,
+//     email: "sanju@varma.com",
+//     hello: function(){ 
+//           return `${this.name} is ${this.age}`
+//     }
+//  };
+//     console.log(typeof(person)); 
+//     console.log(person);
+//     console.log(person.name);
+//     console.log(person.hello);
+//     console.log(person["age"]);
+
+//      let x = "email";
+//     console.log(person[x]);
+
+
+// 2. creating a object 
+
+// let test = new Object();
+// test.name = "Hello";
+// test.email = "Rani@Raja.in"
+// test.abc = function(){
+
+//  }
+// console.log('Rani or Raja');
+
+// test.abc();  
+// delete test.email;
+
+// console.log(test);
+
+
+
+// 3. Nested Object
+//  let person = { name: "Prem",
+//                 age: 25, 
+//                 email: "Prem@Sahay.in",
+//                 address: {
+//                     city: "Surat",
+//                     state: "Gujarat",
+//                     country: "India"
+//                     }
+//                 }
+                // console.log(person.address.city);
+                // console.log(person);
+                // console.log(address);
+
+
+// 4. assign method
+// let person = {
+//                  name: "Skill",
+//                  age: 10,
+//                  email: "hello@test.com",
+//  };
+//  let abc = {};
+//                  Object.assign(abc, person);
+
+//         console.log(abc);
