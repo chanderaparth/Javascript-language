a4891eeb95cfb9116d5ff4e55349e3466fa45a5d
parth <chnderachanderaparth986@gmail.com>
2023-09-15 16:52:17 +0530

    js

---
+++ fun baki/ex1.js
@@ -0,0 +1,87 @@
+// class //
+
+// class data {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     dom(){
+//          return("How Are You.......?");
+//     }
+//     newage(){
+//         return this.age + 20;
+//     }
+// };
+//  let c = new data('viki' , 20);
+
+//  console.log(c.name);
+//  console.log(c.age);
+//  console.log(c.dom());
+//  console.log(c.newage());
+
+
+
+// rest opretar -> ... //
+// rest opretar using by like this array //
+
+// function data(...args) {
+//     var j = 2;
+//     for (let i of args) {
+//         j *= i;
+//     }
+//     return j;
+// }
+
+//   a = data(20, 50);
+//   console.log(a);
+
+//   let dom1 = [5,4,6];
+//   let dom2 = [...dom1];
+//   console.log(dom1);
+//   console.log(dom2);
+
+// dom1.push(5);
+// console.log(dom1);
+// console.log(dom2);
+
+// let a = { s:20 , t:40 }
+// let b = { u:60 }
+// let c = {a, ...b}
+// console.log(c);
+
+// a.s = 10;
+// console.log(a);
+// console.log(c);
+
+
+
+// Function Generator //
+
+// function* generator(){
+ 
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     return 4;
+// }
+ 
+//  let hello = generator();
+//  console.log(hello.next().value);
+//  console.log(hello.next().value); 
+//  console.log(hello.next().value); 
+//  console.log(hello.next().value); 
+
+
+
+// function Call Method //
+
+
+
+
+
+
+
+
+
+
