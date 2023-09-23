9053d57839ea8cee0bfb7486b2c1f1d13b0076b9
parth <chnderachanderaparth986@gmail.com>
2023-09-23 17:28:48 +0530

    js

--- task pogram/clock.js
+++ task pogram/clock.js
@@ -1,19 +1,17 @@
-function WhatTimeIsIt(){
-       let clock = new Date();
-       let hour= clock.getHours();
-       if(hour <10){ hour = "0"+hour}
-       let mn = clock.getMinutes();
-      if(mn <10){ mn = "0"+mn}
-      let sec = clock.getSeconds();
-      if(sec <10){ sec = "0"+sec}
-      let timeIs = hour +  " : " + mn + " : " + sec;
-      document.getElementById("time").innerText = timeIs;
-    let day = clock.getDate();
-    if(day < 10){ day = "0"+day;}
-    let month = (clock.getMonth()) + 1;
-    if (month < 10) { month = "0"+month;}
-    let year = clock.getFullYear();
-    let date = day + " - " + month+ " - " + year;
-    document.getElementById("date").innerText = date;
+function TimeIsIt(){
+    let clock = new Date();
+
+    let hour= clock.getHours();
+    if(hour <10){ hour = "0"+hour}
+
+    let mn = clock.getMinutes();
+    if(mn <10){ mn = "0"+mn}
+
+    let sec = clock.getSeconds();
+    if(sec <10){ sec = "0"+sec}
+
+    let timeIs = hour +  " : " + mn + " : " + sec;
+    document.getElementById("time").innerText = timeIs;
+
   }
-  setInterval(WhatTimeIsIt,1000);
+  setInterval(TimeIsIt,1000);
